1e01f7680f53503daaccd881b50eaf2e1187ce45
Jacob Poling <polingj999@gmail.com>
2023-08-03 05:46:33 -0400

    Created Test GET method

--- api.py
+++ api.py
@@ -1,3 +1,15 @@
 from flask import *
 from flask import jsonify
 
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def test_method():
+    response = {"text": "YOU GOT THE RESPONSE FROM THE BACKEND"}
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(port=8888, host="0.0.0.0")
