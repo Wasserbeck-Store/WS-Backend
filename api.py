ea9a0e4f6a5623d0f42a810464e5c1d30c07cee2
Jacob Poling <polingj999@gmail.com>
2023-08-22 20:05:49 -0400

    Updated Fake DB

--- api.py
+++ api.py
@@ -11,20 +11,20 @@ class Product:
 
 
 fakeDB = {
-    "leatherwork":{"000" : Product("Image go here", "Leather Wallet", "This is a Leather Wallet", 20.00),
-                "001" : Product("Image go here", "Leather Wallet", "This is not Leather Wallet", 21.00),
-                "002" : Product("Image go here", "Leather Wallet", "This is a Leather Wallet", 22.00),
-                "003" : Product("Image go here", "Leather Wallet", "This is a Leather Wallet", 23.00),
+    "Leather Work":{"000" : Product("Image go here", "Leather Wallet", "This is a wallet made of leather. You can tell its made of leather by the way its made of leather. The leather wallet knows its made of leather by the way it feels like leather.", 20.00),
+                "001" : Product("Image go here", "Leather Wallet", "This is a wallet made of leather. You can tell its made of leather by the way its made of leather. The leather wallet knows its made of leather by the way it feels like leather.", 21.00),
+                "002" : Product("Image go here", "Leather Wallet", "This is a wallet made of leather. You can tell its made of leather by the way its made of leather. The leather wallet knows its made of leather by the way it feels like leather.", 22.00),
+                "003" : Product("Image go here", "Leather Wallet", "This is a wallet made of leather. You can tell its made of leather by the way its made of leather. The leather wallet knows its made of leather by the way it feels like leather.", 23.00),
                 },
-    "dndminis":{"000" : Product("Image go here", "DnD Figure", "This is a plastic Mini", 20.00),
-                "001" : Product("Image go here", "DnD Figure", "This is a plastic Mini", 21.00),
-                "002" : Product("Image go here", "DnD Figure", "This is a plastic Mini", 22.00),
-                "003" : Product("Image go here", "DnD Figure", "This is a plastic Mini", 23.00),
+    "D&D Minis":{"000" : Product("Image go here", "DnD Figure", "This is a plastic Mini This is a plastic Mini This is a plastic Mini This is a plastic Mini This is a plastic Mini", 20.00),
+                "001" : Product("Image go here", "DnD Figure", "This is a plastic Mini This is a plastic Mini This is a plastic Mini This is a plastic Mini This is a plastic Mini", 21.00),
+                "002" : Product("Image go here", "DnD Figure", "This is a plastic Mini This is a plastic Mini This is a plastic Mini This is a plastic Mini This is a plastic Mini", 22.00),
+                "003" : Product("Image go here", "DnD Figure", "This is a plastic Mini This is a plastic Mini This is a plastic Mini This is a plastic Mini This is a plastic Mini", 23.00),
                 },
-    "3Dprints":{"000" : Product("Image go here", "3D Printed item", "This is a 3D Printed item", 20.00),
-                "001" : Product("Image go here", "3D Printed item", "This is a 3D Printed item", 21.00),
-                "002" : Product("Image go here", "3D Printed item", "This is a 3D Printed item", 22.00),
-                "003" : Product("Image go here", "3D Printed item", "This is a 3D Printed item", 23.00),
+    "3D Prints":{"000" : Product("Image go here", "3D Printed item", "This is a 3D Printed item This is a 3D Printed item This is a 3D Printed item This is a 3D Printed item This is a 3D Printed item", 20.00),
+                "001" : Product("Image go here", "3D Printed item", "This is a 3D Printed item This is a 3D Printed item This is a 3D Printed item This is a 3D Printed item This is a 3D Printed item", 21.00),
+                "002" : Product("Image go here", "3D Printed item", "This is a 3D Printed item This is a 3D Printed item This is a 3D Printed item This is a 3D Printed item This is a 3D Printed item", 22.00),
+                "003" : Product("Image go here", "3D Printed item", "This is a 3D Printed item This is a 3D Printed item This is a 3D Printed item This is a 3D Printed item This is a 3D Printed item", 23.00),
                 },
 }
 
